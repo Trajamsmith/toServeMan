f2c0db2525afbe8439fd0f27a8f162823f302604
Travis James Smith <trajamsmith@gmail.com>
2018-08-04 18:58:22 -0500

    Add views

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,1 +1,13 @@
 from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+class UserSerializer(zerializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Userfields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
