08877be5dcb04afe6ee3b322d76bd21dcccd6ffd
Travis James Smith <trajamsmith@gmail.com>
2018-08-04 19:17:33 -0500

    Fix typo in serializers

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -4,7 +4,8 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Userfields = ('url', 'username', 'email', 'groups')
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
